224386c34d2ae9048965c04fe7d6c12eb9239bf0
Michael Standarski <mstandarski@gmail.com>
2019-01-02 12:55:08 -0500

    converted to Angular 7

---
+++ djfarms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
